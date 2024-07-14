76ffe515d09aaea7c44dddcda73f804073245495
Liam Cleggett <lbtdgamer@gmail.com>
2024-07-14 16:17:22 +0100

    Basic 2-number addition

--- CleggCalc-P.py
+++ CleggCalc-P.py
@@ -1,2 +1,12 @@
+#Var definition
+numList = [] #list of numbers to use
+result = 0 #result of calculation
+
+#Intro (TODO: make into menu)
 print("This is CleggCalc-P: a coding exercise in Python to make a calculator!")
-print("...Not much here now, but")
+
+#Number input
+numList.append(int(input("Input a number: ")))
+numList.append(int(input("Input another number: ")))
+result = numList[0] + numList[1]
+print("The result is %d!" % result)
