2f9470e3f9e51b42683cb6fea4f276664b8ed6ac
Liam Cleggett <lbtdgamer@gmail.com>
2025-05-21 15:45:47 +0100

    For managing calculation lists/writing them to a readable string

---
+++ ListManage.py
@@ -0,0 +1,14 @@
+import math 
+import csv
+
+logFileName = "CalcLog.csv" #file to save to whenever calculations are done
+
+def PrintLog(): #For printing results from log file
+    with open (logFileName, "r") as logFile:
+        calcHistory = csv.reader(logFile)
+        a = 1
+        for currentRow in calcHistory:
+            print(str(a) + ": " + ",".join(currentRow)) # comma and "join" is to make it look cleaner, separate each piece of data out
+            a += 1
+    print("test")
+    return
